11d4c4b99a20b9f7a1a2057f20f9a85435832d5b
Zach Upstone <zu213@bath.ac.uk>
2025-03-16 11:45:12 +0000

    Added inital random games to solve

---
+++ rngs.py
@@ -0,0 +1,62 @@
+import random
+import secrets
+
+#C:\Users\evilm\AppData\Local\Programs\Python\Python311\python.exe
+
+def not_random_repeating():
+    return 1
+
+def not_random_addition():
+    if not hasattr(not_random_addition, "last_value"):
+        not_random_addition.last_value = 0
+
+    not_random_addition.last_value += 3
+    return not_random_addition.last_value
+
+def not_random_multiplication():
+    if not hasattr(not_random_addition, "last_value"):
+        not_random_addition.last_value = 1
+
+    not_random_addition.last_value *= 4
+    return not_random_addition.last_value
+
+def not_random_power():
+    if not hasattr(not_random_addition, "last_value"):
+        not_random_addition.last_value = 1
+
+    not_random_addition.last_value *= not_random_addition.last_value
+    return not_random_addition.last_value
+
+def pseudo_random_lcg(seed, a=14, c=111, m=2**8):
+    return (a * seed + c) % m
+
+def pseudo_random_middle_square(seed):
+    squared = seed ** 2
+    while(squared > 100):
+        int(str(squared)[1:])
+        squared = squared // 10
+    
+    return squared
+    
+def pseudo_random_mersenne(seed):
+    if not hasattr(pseudo_random_mersenne, "seed"):
+        not_random_addition.seed = seed
+        random.seed(not_random_addition.seed)
+        
+    return random.randint(1, 100)
+    
+def pseudo_random_prng():
+    return secrets.randbelow(100)
+
+def main():
+    print(not_random_repeating())
+    print(not_random_addition())
+    print(not_random_multiplication())
+    print(not_random_power())
+    print(pseudo_random_lcg(55))
+    print(pseudo_random_middle_square(55))
+    print(pseudo_random_mersenne(55))
+    print(pseudo_random_prng())
+    
+if __name__ == "__main__":
+    main()
